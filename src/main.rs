34584c7169293896c032c86bdb2bfc969980816c
ehsan <ehsan2003.2003.382@gmail.com>
2022-05-28 15:25:46 +0430

    adds feature to extract text in a more flexible way

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use clap::Parser;
-use cli_selector::{extract, ExtractOptions};
+
+use scraper::ElementRef;
 use std::{error::Error, io::Read, path::PathBuf};
 
 #[derive(clap::Parser)]
 #[clap(author, version, about, long_about = None)]
-pub struct Args {
+pub struct Arguments {
     /// input file ( specify "-" for stdin )
     #[clap(
         short,
@@ -22,17 +23,60 @@ pub struct Args {
     #[clap(long, short,default_value_t = String::from("\n"))]
     separator: String,
 
-    #[clap(long, short = 'n')]
-    inner: bool,
+    #[clap(subcommand)]
+    function: Option<ExtractionFunction>,
+}
+
+#[derive(clap::Subcommand)]
+pub enum ExtractionFunction {
+    /// extracts attribute from html tags matched by selector
+    Attr(Attribute),
+    /// inner html of matched tags
+    Inner,
+    /// inner text of matched tags
+    Text,
+    /// outer html of matched tags
+    Outer,
+}
+impl Default for ExtractionFunction {
+    fn default() -> Self {
+        Self::Outer
+    }
+}
+#[derive(clap::Args)]
+pub struct Attribute {
+    attr: String,
+}
+impl ExtractionFunction {
+    pub fn extract(&self, element: ElementRef) -> String {
+        match self {
+            ExtractionFunction::Attr(Attribute { attr }) => element
+                .value()
+                .attr(attr)
+                .to_owned()
+                .unwrap_or_default()
+                .to_string(),
+            ExtractionFunction::Inner => element.inner_html(),
+            ExtractionFunction::Outer => element.html(),
+            ExtractionFunction::Text => element.text().collect(),
+        }
+    }
+}
+pub struct ExtractOptions {
+    pub input: String,
+    pub selector: scraper::Selector,
+    pub separator: String,
+    pub extraction_function: ExtractionFunction,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let Args {
+    let Arguments {
         input: input_file,
         selector,
         separator,
-        inner,
-    } = Args::parse();
+        function,
+    } = Arguments::parse();
+    let function = function.unwrap_or_default();
     let selector = scraper::Selector::parse(&selector).expect("selector parse error");
 
     let str = if input_file.eq(&PathBuf::from("-")) {
@@ -43,12 +87,12 @@ fn main() -> Result<(), Box<dyn Error>> {
         std::fs::read_to_string(input_file)?
     };
 
-    let out = extract(ExtractOptions {
-        input: str,
-        selector,
-        separator,
-        inner,
-    });
+    let out = scraper::Html::parse_fragment(&str)
+        .root_element()
+        .select(&selector)
+        .map(|elem| function.extract(elem))
+        .collect::<Vec<_>>()
+        .join(&separator);
 
     println!("{}", out);
 
